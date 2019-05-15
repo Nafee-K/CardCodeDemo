ef15413e43cf6691e940e2c67f3cae739b082aa0
Ali Hassan <ali.hassan1@sheridanc.on.ca>
2019-05-15 16:24:49 -0400

    This is my first commit

---
+++ src/Week2/SoftwareFundamentals/Card.java
@@ -0,0 +1,62 @@
+package Week2.SoftwareFundamentals;
+
+/**
+ * A simple file to model Card class.
+ *
+ * @author Ali Hassan, May 15 2019
+ */
+public class Card {
+
+    private String suit;//Hearts, Spades, Clubs, Diamonds
+    private int value; //1-13
+
+    /**
+     * A constructor to instantiate Card class.
+     *
+     * @param suit
+     * @param value
+     */
+    public Card(String suit, int value) {
+        if (suit.equalsIgnoreCase("Hearts") || suit.equalsIgnoreCase("Clubs")
+                || suit.equalsIgnoreCase("Spades") || suit.equalsIgnoreCase("Diamonds")) {
+            this.suit = suit;
+        } else {
+            System.out.println("Invalid suit, try again");
+        }
+        if (value < 1 || value > 13) {
+            System.out.println("Invalid value");
+        } else {
+            this.value = value;
+        }
+
+    }
+
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
---
+++ src/Week2/SoftwareFundamentals/CardDemo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week2.SoftwareFundamentals;
+
+/**
+ * A simple simulator to Demonstrate cards.
+ *
+ * @author Ali Hassan, May 15th 2019
+ */
+public class CardDemo {
+
+    public static void main(String[] args) {
+        Card c1 = new Card("Hearts", 10);
+        Card c2 = new Card ("Hello World", -100);
+    }
+
+}
